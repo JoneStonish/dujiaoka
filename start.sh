4dcfc8a3917aefb410119efcc788816d54bda256
JoneStonish <117419185+JoneStonish@users.noreply.github.com>
2023-06-18 20:53:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,6 +17,9 @@ if [ -f "/dujiaoka/.env" ]; then
     php artisan migrate
 
     supervisord
+
+    cat /opt/docker/etc/supervisor.d/dujiao.conf
+    cat /opt/docker/etc/supervisor.d/nginx.conf
 else
     echo "配置文件不存在，请根据文档修改配置文件！"
 fi
